db2dd5a65a7681293afb49f7e65fc708e3d4f6d4
Abishe1 <abisheguma@gmail.com>
2022-10-18 09:07:50 -0700

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
-#include <unistd.h>
-/**
- *putchar - writes the character c to stdout
- *@c: the character to print
- *Return: on success 1.
- *On error, -1 is returned, and erron is set appropriately.
- */
-int -putchar(char c)
-{
-return (write(1, &c, 1));
+#include "main.h"                                                                                                                
+/**                                                                                                                                      
+ * main - prints putchar                                                                                                                 
+ * Return: always 0                                                                                                                      
+ */                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                        
+ _putchar('_');                                                                                                                         
+ _putchar('p');                                                                                                                          
+ _putchar('u');                                                                                                                          
+ _putchar('t');                                                                                                                          
+ _putchar('c');                                                                                                                          
+ _putchar('h');                                                                                                                          
+ _putchar('a');                                                                                                                          
+ _putchar('r');                                                                                                                          
+_putchar('\n');                                                                                                                        
+return (0);
 }
-

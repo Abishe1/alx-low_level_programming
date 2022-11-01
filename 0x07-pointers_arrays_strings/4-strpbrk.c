bc4da3d42a68b7ee449df2a4ac2cf80d2aca375f
Abishe1 <abisheguma@gmail.com>
2022-11-01 10:38:00 -0700

    task 12

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_strpbrk located the frist occurence in S if any bytes in accept
+ *_strpbrk located the frist occurence in s if any bytes in accept
  *@s: the string we look for occurenced in
  *@accept: the print we compare against
  *Return: a pointer to the byte or null
  */
-char *_strstr(char *haystack, char *needle)
+char *_strpbrk(char *s, char *accept);
 {
 char c, *p;
 for (c = *S; c != 0; S++, c = *S)
@@ -15,7 +15,7 @@ for (p = accept; *p != 0; p++)
 if (c == *p)
 {
 return (s);
-} 
+}
 }
 }
 return (0);

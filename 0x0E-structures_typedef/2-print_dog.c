90f068532e03602911de919b425f833bb4c66751
Abishe1 <abisheguma@gmail.com>
2022-11-14 09:07:45 -0800

    task 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,8 @@ else
 printf("Name: %s\n", d->name);
 if (d->age < 0)
 printf("Age: (nil)\n");
-elsprintf("Age: %f\n", d->age);
+else
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else

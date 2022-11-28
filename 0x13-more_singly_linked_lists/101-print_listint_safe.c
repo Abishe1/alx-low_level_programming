ac0322a7ba2fc462bb81834182d42eea146b82b7
Abishe1 <abisheguma@gmail.com>
2022-11-28 12:05:54 -0800

    task 122

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -42,7 +42,7 @@ return (0);
 }
 /**
  * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t *head)
+ * @head: A pointer to the head of the listint_t list.
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
@@ -52,6 +52,7 @@ nodes = looped_listint_len(head);
 if (nodes == 0)
 {
 for (; head != NULL; nodes++)
+{
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
@@ -67,3 +68,4 @@ printf("-> [%p] %d\n", (void *)head, head->n);
 }
 return (nodes);
 }
+
